4eddf5451ce15dea0eeae6b505130d60bf3f4db9
cantski <164589592+cantski@users.noreply.github.com>
2024-03-24 17:29:50 +1100

    Add ChicagoFLF for iPod UI

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,12 @@
 import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const chicago = localFont({
+  src: "./fonts/chicago/ChicagoFLF.ttf",
+  variable: "--font-chicago",
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} ${chicago.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
